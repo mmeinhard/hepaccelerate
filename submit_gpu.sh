374ae550605d35455cb0bbba430e58407d6c5f0b
Daniele Ruini <daniele.ruini@cern.ch>
2019-07-19 14:04:32 +0200

    add scripts to submit slurm jobs for cpu and gpu

--- submit_gpu.sh
+++ submit_gpu.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
-
-#SBATCH --job-name=gpu_analysis                 
-
-#SBATCH --account=gpu_gres  # to access gpu resources
-
-#SBATCH --nodes=1       # request to run job on single node                                       
-
-#SBATCH --ntasks=5     # request 10 CPU's (t3gpu01: balance between CPU and GPU : 5CPU/1GPU)      
-
-#SBATCH --gres=gpu:1    # request 1 GPU's on machine                                         
-
 echo "start running python script"
 
-PYTHONPATH=.:$PYTHONPATH python3 simple_ttH.py --filelist datasets/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8.txt --sample ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8 --from-cache --evaluate-DNN --use-cuda
+mc_samples=(
+ttHTobb_M125_TuneCP5_13TeV-powheg-pythia8
+ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8
+TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8
+TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8
+TTToHadronic_TuneCP5_13TeV-powheg-pythia8
+)
+for sample in ${mc_samples[@]}; do
+  echo "submitting sample $sample"
+  sbatch gpu_job.sh $sample
+  #PYTHONPATH=hepaccelerate:coffea:. python3 run_analysis.py --filelist datasets/RunIIFall17NanoAODv4/$sample.txt --sample $sample  --outdir results_studyBoostedStatistics/mc --from-cache --boosted --use-cuda #--DNN save-arrays --cache-location /scratch/druini/cache/ 
+  #python run_analysis.py --filelist datasets/RunIIFall17NanoAODv4/$sample.txt --sample $sample  --cache-location /scratch/druini/cache/ --outdir /scratch/druini/results_studyBoostedStatistics/mc --from-cache --boosted #--DNN save-arrays
+done
 
 echo "done"
